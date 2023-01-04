2bda33e51d661caec58453ab3f2e3f19096168a5
Elena Korenko <elenakor987@gmail.com>
2023-01-04 23:47:58 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задайте значения M и N.
+Напишите программу, которая найдёт сумму натуральных элементов в 
+промежутке от М до N.
+M = 1;N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int ShowNumbers(int m, int n)
+{
+    if(m - n <= 0)
+  {
+    return m + ShowNumbers(m + 1, n);
+  }
+  return 0;
+} 
+
+Console.Write(ShowNumbers(2, 5));
